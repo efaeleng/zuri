bd6f7864f1fb123c48506479b6b456336263a0c1
Efa-iwa Eleng <gistend@gmail.com>
2022-06-22 17:54:19 +0100

    JS Calculator

--- js_calculator/index.js
+++ js_calculator/index.js
@@ -1,38 +1,52 @@
-// function to perform simple arithmetic operations
+// Basic JS Calculator
 
-// take the operator input
-const operator = prompt('Welcome!!! please enter an operator sign: \n\n" + " :  Addition.\n " - " :   Subtraction.\n " * " :   Multiplication.\n " / " :   Division.');
 
-// take the operand input
-const number1 = parseFloat(prompt('First number: '));
-const number2 = parseFloat(prompt('Second number: '));
+const addition =(a, b)=>{
+  return a + b
+}
+const subtraction=(a, b)=>{
+  return a - b
+}
+const multiplication=(a, b)=>{
+  return a * b
+}
 
-let result = 0
-let displayCalculations = ""
-const calculations = (number1, number2) => {
-    result = number1 + number2;
-    // using if...else if... else
-    if (operator == '+') {
-        result = number1 + number2;
-        return result
-    }
-    else if (operator == '-') {
-        result = number1 - number2;
-    }
-    else if (operator == '*') {
-        result = number1 * number2; 
-    }
-    else if ( operator == '/' ) {
-        result = number1 / number2;
-    }
-    else{
-      print('Enter the right Operator')
-    }
+const division=(a, b)=>{
+  return a / b
+}
 
+while (true) {
+  const question = Number(prompt('Welcome to my calculator!!!.\n Press 1 for addition.  \n Press 2 for subtraction. \n Press 3 for multiplication. \n Press 4 for division. '))
+
+  if (question === 1) {
+      let a = Number(prompt('First number'))
+      let b = Number(prompt('Second number'))
+      alert(addition(a, b))
+      break
   }
-  displayCalculations = `The Result of ${number1} ${operator} ${number2} is : ${result} `
-  
-  alert(displayCalculations);
-  
-//Displaying the result
 
+  else if (question === 2) {
+      let a = Number(prompt('First number'))
+      let b = Number(prompt('Second number'))
+      alert(subtraction(a, b))
+      break
+  }
+
+  else if (question === 3) {
+      let a = Number(prompt('First number'))
+      let b = Number(prompt('Second number'))
+      alert(multiplication(a, b))
+      break
+  }
+
+  else if (question === 4) {
+      let a = Number(prompt('First number'))
+      let b = Number(prompt('Second number'))
+      alert(division(a, b))
+      break
+  }
+
+  else {
+      alert('Wrong input.Please try again!!!')
+  }    
+}
